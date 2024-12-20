5be5c0ef7a032a8c9544471b4c9e2e3be2532052
Raffie <raffiesaurus@gmail.com>
2024-12-20 23:33:32 +0000

    Starter Content

---
+++ Source/RhythmHell/RhythmHell.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RhythmHell : ModuleRules
+{
+	public RhythmHell(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
